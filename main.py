f4e99ea478e87359e36df06417e4f44c23d12fdf
kmkmi <jk787787@naver.com>
2020-08-24 22:21:52 +0900

    pycurl

---
+++ main.py
@@ -0,0 +1,57 @@
+import getopt
+import sys
+import requests
+from xss_main import XSS
+
+
+optlist = "g:p:a:"
+
+
+
+def usage():
+    print()
+
+opts, args = getopt.getopt(sys.argv[1:], optlist)
+
+
+url = ""
+path = args[0]
+
+params = {}
+
+
+for i, j in opts:
+    if i == "-g":
+        url, getParams = j.split('?',1)
+
+        plist = getParams.split('&')
+
+        for i in range(len(plist)):
+            pm, vl = plist[i].split('=',1)
+            params[pm] = vl
+
+        get_xss = XSS("GET", url, params, path)
+        get_xss.StartFuzz()
+
+
+    elif i == "-p":
+
+        url = j
+
+        plist =[]
+        for a, postParams in opts:
+            if a == "-a":
+                plist = postParams.split('&')
+
+
+        #print(plist)
+        for i in range(len(plist)):
+            pm, vl = plist[i].split('=',1)
+            params[pm] = vl
+
+        get_xss = XSS("POST", url, params, path)
+        get_xss.StartFuzz()
+
+
+    else:
+        usage()
