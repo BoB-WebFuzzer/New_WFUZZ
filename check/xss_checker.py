9288164fcbaae7e77dcf75148595a34f34a4813d
jej2660 <juj2660@naver.com>
2020-08-26 07:01:14 +0900

    add_check

--- check/xss_checker.py
+++ check/xss_checker.py
@@ -13,11 +13,24 @@ class Checkseed():
         self.chrome_opt.add_argument("--incognito")
         #self.chrome_options.add_argument("--headless")
         self.driver = webdriver.Chrome('./chromedriver', chrome_options=self.chrome_opt)
+    def preprocess(self):
+        fd = open("test.html", "r")
+        fp = open("test2.html", "w")
+        string = fd.readline()
+        string = "<script>location.reload = () => {}; window.testSuccess = false; window.test = () => testSuccess = true;</script>" + "\n" + string
+        fp.writelines(string)
 
     def checking(self):
-        self.driver.get("file://"+os.getcwd()+'/'+"test.html")
-        tes = self.driver.execute_script("return typeof conf =='undefined'")
-        print(tes)
+        self.preprocess()
+        self.driver.get("file://"+os.getcwd()+'/'+"test2.html")
+        tes = self.driver.execute_script("return window.testSuccess")
+        return tes
+
+# fd = open("test.html","r")
+# fp = open("test2.html","w")
+# string = fd.readline()
+# string =  "<script>location.reload = () => {}; window.testSuccess = false; window.test = () => testSuccess = true;</script>" + "\n" +string
+# fp.writelines(string)
 ch = Checkseed()
 ch.checking()
 
